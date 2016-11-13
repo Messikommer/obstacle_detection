9d1b2af766ef5b449d95d115408c8c93c7045ddb
Messikommer <nico.messikommer@gmail.com>
2016-11-13 21:34:01 +0100

    nix

---
+++ obstacle_detection.cpp
@@ -0,0 +1,25 @@
+#include <ros/ros.h>
+
+
+
+
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "obstacle_detection");
+  ros::NodeHandle nh;
+  
+  
+  
+  
+  
+  
+  
+  ros::spin();
+
+  return 0;
+}
+
+
+
+
