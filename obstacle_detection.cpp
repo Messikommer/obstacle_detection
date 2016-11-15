0f2454c11bc47e850ffc9909c4b5ff7e3b3b70ce
Messikommer <nico.messikommer@gmail.com>
2016-11-15 15:58:24 +0100

    Third

--- obstacle_detection.cpp
+++ obstacle_detection.cpp
@@ -1,5 +1,33 @@
 #include <ros/ros.h>
+#include <math.h>
+#include "sensor_msgs/PointField.h"
+#include <sensor_msgs/PointCloud2.h>
 
+
+void scan(const sensor_msgs::PointCloud2ConstPtr &PCL2) {
+  float x=PCL2->data[1];
+  float y=PCL2->data[2];
+  float z=PCL2->data[3];
+  float d=sqrt(y*y+x*x);
+  
+  
+  float alpha=atan(z/d);
+  float beta=atan(y/d);
+  
+  if(-16<alpha<16 && -5<beta<5 ) {
+    
+    
+  }
+  
+  else if(-5<beta<5){
+    
+    
+  }
+  
+
+  
+  
+}
 
 
 
@@ -9,7 +37,7 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "obstacle_detection");
   ros::NodeHandle nh;
   
-  
+  ross::
   
   
   
