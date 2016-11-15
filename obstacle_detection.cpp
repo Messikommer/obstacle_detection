43a3d8ee0fd21c530d617d278f3cb54429238a79
Messikommer <nico.messikommer@gmail.com>
2016-11-15 22:02:59 +0100

    First compiling version

--- obstacle_detection.cpp
+++ obstacle_detection.cpp
@@ -1,32 +1,32 @@
 #include <ros/ros.h>
-
 #include <math.h>
-#include "sensor_msgs/PointField.h"
- 
 #include <sensor_msgs/PointCloud2.h>
+
+
+
+ros::Publisher obstacle_pub;
+
+
 
 
 void scan(const sensor_msgs::PointCloud2ConstPtr &PCL2) {
   float x=PCL2->data[1];
   float y=PCL2->data[2];
   float z=PCL2->data[3];
-  float d=sqrt(y*y+x*x);
-  
-  
-  float alpha=atan(z/d);
-  float beta=atan(y/d);
-  
-  if(-16<alpha<16 && -5<beta<5 ) {
-    
-    
-  }
-  
-  else if(-5<beta<5){
-    
-    
-  }
+  float d1=sqrt(y*y+x*x+z*z);
+  float d2=sqrt(y*y+x*x);
   
 
+
+ 
+  
+  float alpha=atan(z/d2);
+  float beta=atan(y/d2);
+  
+  if(-16<alpha<-14 && 0<beta<10 ) {
+    obstacle_pub.publish(PCL2);
+  }
+  else if(-10<beta<10){  }
   
   
 }
@@ -37,18 +37,22 @@ void scan(const sensor_msgs::PointCloud2ConstPtr &PCL2) {
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "obstacle_detection");
-  ros::NodeHandle nh;
+  ros::NodeHandle n;
+
   
-  ross::
-  
+  obstacle_pub = n.advertise<sensor_msgs::PointCloud2>("/obstacles", 1000);
+  ros::Subscriber sub = n.subscribe("/velodyne_points", 1000, scan);
+
   
   
   
   
   ros::spin();
-
   return 0;
 }
+
+//sensor_msgs::PointCloud2 p;
+//p.data[1]=PCL2->data[1];
 
 
 
